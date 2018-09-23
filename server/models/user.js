81ef3f1e656f05d2496e5b930f080fcb198080c9
MikeBee14 <michael.bardouille@prodigygame.com>
2018-09-23 14:09:09 -0400

    Add post user route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,23 +1,54 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('Users', {
+var UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true   
-    }
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }   
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    var userObject = user.toObject();
 
+    return _.pick(userObject, ['_id', 'email']);
+};
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
-// var otherTodo = new User({
-//     email: 'Automation+teacher2017-10-02194727.479@prodigygame.com'
-// });
+    user.tokens = user.tokens.concat({access, token});
+    
+    return user.save().then(() => {
+        return token;
+    });
+};
 
-// otherTodo.save().then((doc) => {
-//     console.log('Saved user', doc);
-// }, (e) => {
-//     console.log('Unable to save todo', e);
-// });
+var User = mongoose.model('Users', UserSchema)
 
 module.exports = {User};
